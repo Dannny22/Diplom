0c760ac0c26cce5a5b4a75fd0953cb8eb1d573a3
Dannny22 <danya.bmw4@gmail.com>
2022-01-09 21:07:54 +0200

    стан и разделение скрипта передвижение врага
    фцвфцвф

--- Work/Assets/Main scripts/Enemy.cs
+++ Work/Assets/Main scripts/Enemy.cs
@@ -8,13 +8,14 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
 
-    public GameObject Player;
-    public float dist;
-    NavMeshAgent nav;
-    public float Radius = 15;
+    //public GameObject Player;
+    //public float dist;
+    //NavMeshAgent nav;
+    //public float Radius = 15;
     public float HP = 100;
     public GameObject Ragdoll;
     public float BurnTime = 0f;
+    public float StunTime = 0f;
     //PlayerControll playercontroll = new PlayerControll();
     //public Animator anim;
     public static bool Attack;
@@ -32,7 +33,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        nav = GetComponent<NavMeshAgent>();
+        //nav = GetComponent<NavMeshAgent>();
     }
 
 
@@ -60,35 +61,35 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Player = GameObject.FindGameObjectWithTag("Player");
-        dist = Vector3.Distance(Player.transform.position, transform.position);
+        //Player = GameObject.FindGameObjectWithTag("Player");
+        //dist = Vector3.Distance(Player.transform.position, transform.position);
 
-        if (dist > Radius)
-        {
-            //anim.SetBool("AttackEnemy", false);
-            nav.speed = 2;
-            gameObject.GetComponent<Animator>().SetTrigger("walk");
-            gameObject.GetComponent<EnemyPatrol>().enabled = true;
-            gameObject.GetComponent<Animator>().SetBool("run", false);
-        }
+        //if (dist > Radius)
+        //{
+        //    //anim.SetBool("AttackEnemy", false);
+        //    nav.speed = 2;
+        //    gameObject.GetComponent<Animator>().SetTrigger("walk");
+        //    gameObject.GetComponent<EnemyPatrol>().enabled = true;
+        //    gameObject.GetComponent<Animator>().SetBool("run", false);
+        //}
 
-        if (dist < Radius && dist > 1.5f)
-        {
+        //if (dist < Radius && dist > 1.5f)
+        //{
 
-            nav.speed = 5;
-            gameObject.GetComponent<EnemyPatrol>().enabled = false;
-            gameObject.GetComponent<Animator>().SetBool("run", true);
-            nav.enabled = true;
-            nav.SetDestination(Player.transform.position);
-        }
-        if (dist < 1f)
-        {
-            gameObject.GetComponent<Animator>().SetTrigger("attack");
-            nav.enabled = false;
-            AttackEnemy = true;
-            Invoke("DiactivetAttack", 1f);
+        //    nav.speed = 5;
+        //    gameObject.GetComponent<EnemyPatrol>().enabled = false;
+        //    gameObject.GetComponent<Animator>().SetBool("run", true);
+        //    nav.enabled = true;
+        //    nav.SetDestination(Player.transform.position);
+        //}
+        //if (dist < 1f)
+        //{
+        //    gameObject.GetComponent<Animator>().SetTrigger("attack");
+        //    nav.enabled = false;
+        //    AttackEnemy = true;
+        //    Invoke("DiactivetAttack", 1f);
 
-        }
+        //}
         if (isSuperCombo)
         {
             powerUpResetTimer -= Time.deltaTime;
@@ -129,12 +130,30 @@ public class Enemy : MonoBehaviour
 
         if (other.tag == "Skill2")
         {
+            StunTime = 5f;
+            StartCoroutine(Stunning());
+            gameObject.GetComponent<Animator>().SetTrigger("stun");
             
-            HP = HP - 50f;
-
+            HP = HP - 20f;
+            
         }
-
+        if (StunTime <= 0)
+        {
+            gameObject.GetComponent<EnemyMovement>().enabled = true;
+        }
     }
+
+    private IEnumerator Stunning()
+    {
+        while (StunTime > 0)
+        {
+            StunTime = StunTime - 1f;
+            gameObject.GetComponent<EnemyMovement>().enabled = false;
+            gameObject.GetComponent<NavMeshAgent>().enabled = false;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
     private IEnumerator BurnDamage()
     {
         while (BurnTime > 0)
---
+++ Work/Assets/Main scripts/EnemyMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public GameObject Player;
+    public float dist;
+    NavMeshAgent nav;
+    public float Radius = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void DiactivetAttack()
+    {
+        Enemy.AttackEnemy = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        dist = Vector3.Distance(Player.transform.position, transform.position);
+
+        if (dist > Radius)
+        {
+            //anim.SetBool("AttackEnemy", false);
+            nav.speed = 2;
+            gameObject.GetComponent<Animator>().SetTrigger("walk");
+            gameObject.GetComponent<EnemyPatrol>().enabled = true;
+            gameObject.GetComponent<Animator>().SetBool("run", false);
+        }
+
+        if (dist < Radius && dist > 1.5f)
+        {
+
+            nav.speed = 5;
+            gameObject.GetComponent<EnemyPatrol>().enabled = false;
+            gameObject.GetComponent<Animator>().SetBool("run", true);
+            nav.enabled = true;
+            nav.SetDestination(Player.transform.position);
+        }
+        if (dist < 1f)
+        {
+            gameObject.GetComponent<Animator>().SetTrigger("attack");
+            nav.enabled = false;
+            Enemy.AttackEnemy = true;
+            Invoke("DiactivetAttack", 1f);
+
+        }
+    }
+}
