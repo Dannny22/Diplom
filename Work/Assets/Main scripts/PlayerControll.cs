c6cf73b67c8c45ce7cf165568a46e025a9fbf59b
Dannny22 <danya.bmw4@gmail.com>
2022-01-14 22:00:47 +0200

    Иконки скилов и кд на гл. экране

--- Work/Assets/Main scripts/PlayerControll.cs
+++ Work/Assets/Main scripts/PlayerControll.cs
@@ -30,6 +30,10 @@ public class PlayerControll : MonoBehaviour
     public float Mana = 1f;
     public Image UIMana;
     public int time = 0;
+    //public Image UISkill1;
+    //public Image UISkill2;
+    public Text UISkill1Time;
+    public Text UISkill2Time;
 
     // Start is called before the first frame update
     void Start()
@@ -46,11 +50,14 @@ public class PlayerControll : MonoBehaviour
     void Update()
     {
         UIMana.fillAmount = Mana;
-        CountdownSkill1 -= Time.deltaTime;
-        CountdownSkill2 -= Time.deltaTime;
         Mana += Time.deltaTime / 25f;
         anim.SetBool("Jump", false);
-        
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            gameObject.GetComponent<Animator>().SetTrigger("Roll");
+        }
+
         if (Mana > 1f)
         {
             Mana = 1f;
@@ -58,17 +65,32 @@ public class PlayerControll : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Alpha1) & CountdownSkill1 <= 0 & Mana >= 0.25f)
         {
+            
             Instantiate(Skill1, transform.position, transform.rotation);
-            CountdownSkill1 = 5f;
+            CountdownSkill1 = 6f;
             Mana = Mana - 0.25f;
+            StartCoroutine(Skill1Time());
         }
+        if (CountdownSkill1 <= 0)
+        {
+            //UISkill1.gameObject.SetActive(false);
+            UISkill1Time.gameObject.SetActive(false);
+        }
+
 
         if (Input.GetKeyDown(KeyCode.Alpha2) & CountdownSkill2 <= 0 & Mana >= 0.5f)
         {
             Instantiate(Skill2, transform.position, transform.rotation);
-            CountdownSkill2 = 10f;
+            CountdownSkill2 = 11f;
             Mana = Mana - 0.5f;
+            StartCoroutine(Skill2Time());
         }
+        if (CountdownSkill2 <= 0)
+        {
+            UISkill2Time.gameObject.SetActive(false);
+        }
+
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Enemy.Attack = true;
@@ -108,7 +130,32 @@ public class PlayerControll : MonoBehaviour
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-
-
     }
+
+    private IEnumerator Skill1Time()
+    {
+        while (CountdownSkill1 > 0)
+        {
+            CountdownSkill1 = CountdownSkill1 - 1f;
+            //UISkill1.gameObject.SetActive(true);
+            UISkill1Time.gameObject.SetActive(true);
+            UISkill1Time.text = "" + CountdownSkill1;
+            //UISkill1.fillAmount = CountdownSkill1 / 2.5f;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    private IEnumerator Skill2Time()
+    {
+        while (CountdownSkill2 > 0)
+        {
+            CountdownSkill2 = CountdownSkill2 - 1f;
+            //UISkill1.gameObject.SetActive(true);
+            UISkill2Time.gameObject.SetActive(true);
+            UISkill2Time.text = "" + CountdownSkill2;
+            //UISkill1.fillAmount = CountdownSkill1 / 2.5f;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
 }
