eca8c29afd3d685525e37a8bbf537cde70b4691f
Dannny22 <danya.bmw4@gmail.com>
2022-06-12 00:12:42 +0300

    Меню, меч, асет

---
+++ Work/Assets/Main scripts/Menu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayGame()
+        {
+            SceneManager.LoadScene(0);
+        }
+    
+
+    public void ExitGame()
+        {
+            Debug.Log("Игра закрылась");
+            Application.Quit();
+        }
+}
