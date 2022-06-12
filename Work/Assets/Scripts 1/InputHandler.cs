35c05085238d48a9ac213c5dc867400f902db1ec
Lixerech <Lixerech@gmail.com>
2022-06-12 15:31:57 +0300

    Спринт

--- Work/Assets/Scripts 1/InputHandler.cs
+++ Work/Assets/Scripts 1/InputHandler.cs
@@ -14,6 +14,8 @@ namespace SG
 
         public bool b_Input;
         public bool rollFlag;
+        public bool sprintFlag;
+        public float rollInputTimer;
         public bool isInteracting;
 
         PlayerControls inputActions;
@@ -75,7 +77,18 @@ namespace SG
             b_Input = inputActions.PlayerActions.Roll.phase == UnityEngine.InputSystem.InputActionPhase.Performed;
             if (b_Input)
             {
-                rollFlag = true;
+                rollInputTimer += delta;
+                sprintFlag = true;
+            }
+            else
+            {
+                if (rollInputTimer > 0 && rollInputTimer < 0.5f)
+                {
+                    sprintFlag = false;
+                    rollFlag = true;
+                }
+
+                rollInputTimer = 0;
             }
         }
     }
