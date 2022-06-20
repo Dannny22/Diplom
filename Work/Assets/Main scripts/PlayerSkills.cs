dd929320866b647c37392e62e2f3f83c93b33e4f
Dannny22 <danya.bmw4@gmail.com>
2022-06-20 10:09:30 +0300

    UI

--- Work/Assets/Main scripts/PlayerSkills.cs
+++ Work/Assets/Main scripts/PlayerSkills.cs
@@ -3,21 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerSkills : MonoCache
+public class PlayerSkills : MonoBehaviour
 {
     public GameObject Skill1;
     public GameObject Skill2;
+    //public GameObject Skill3;
     public float CountdownSkill1 = 0f;
     public float CountdownSkill2 = 0f;
+    //public float CountdownSkill3 = 0f;
     public float Mana = 1f;
     public Image UIMana;
     public int time = 0;
     public Text UISkill1Time;
     public Text UISkill2Time;
+    //public Text UISkill3Time;
     public AudioSource Skill1Sound;
     public AudioSource Skill2Sound;
+    //public AudioSource Skill3Sound;
+    //public PlayerStats playerStats;
 
-    public override void OnTick()
+    //private void Awake()
+    //{
+    //    playerStats = GetComponent<PlayerStats>();
+    //}
+
+    public void Update()
     {
         UIMana.fillAmount = Mana;
         Mana += Time.deltaTime / 25f;
@@ -52,7 +62,22 @@ public class PlayerSkills : MonoCache
         {
             UISkill2Time.gameObject.SetActive(false);
         }
-    } 
+
+        //if (Input.GetKeyDown(KeyCode.Alpha3) & CountdownSkill3 <= 0 & Mana >= 0.25f)
+        //{
+        //    Skill3Sound.Play();
+        //    Instantiate(Skill1, transform.position, transform.rotation);
+        //    CountdownSkill1 = 10f;
+        //    Mana = Mana - 0.25f;
+        //    playerStats.currentHealth = playerStats.currentHealth + 25f;
+        //    StartCoroutine(Skill3Time());
+        //}
+        //if (CountdownSkill3 <= 0)
+        //{
+        //    UISkill3Time.gameObject.SetActive(false);
+        //}
+
+    }
     private IEnumerator Skill1Time()
     {
         while (CountdownSkill1 > 0)
@@ -78,4 +103,17 @@ public class PlayerSkills : MonoCache
             yield return new WaitForSeconds(1f);
         }
     }
+
+    //private IEnumerator Skill3Time()
+    //{
+    //    while (CountdownSkill3 > 0)
+    //    {
+    //        CountdownSkill3 = CountdownSkill3 - 1f;
+    //        //UISkill1.gameObject.SetActive(true);
+    //        UISkill3Time.gameObject.SetActive(true);
+    //        UISkill3Time.text = "" + CountdownSkill3;
+    //        //UISkill1.fillAmount = CountdownSkill1 / 2.5f;
+    //        yield return new WaitForSeconds(1f);
+    //    }
+    //}
 }
